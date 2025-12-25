a0d2002c44e487f8aaf624182c2d11f4d404d81b
Jiří Lebduška <jiri.lebduska@gmail.com>
2025-12-25 19:03:05 +0100

    refactor(app): add component barrels and use them in App

---
+++ blendmate-app/src/components/index.ts
@@ -0,0 +1,6 @@
+// Barrel file for components
+export { default as Outliner } from "./Outliner";
+export { default as NodeHelpView } from "./NodeHelpView";
+export * as Panels from "./panels";
+export * from "./ui";
+
---
+++ blendmate-app/src/components/ui/index.ts
@@ -0,0 +1,11 @@
+// Barrel file for ui components
+export { BackgroundPaths, BackgroundPathsProps } from "./BackgroundPaths";
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent, CardAction } from "./Card";
+export { default as IslandPanel } from "./IslandPanel";
+export { ScrollArea, ScrollBar } from "./scroll-area";
+export { ResizablePanelGroup as ResizablePanelGroup, ResizablePanel as ResizablePanel, ResizableHandle as ResizableHandle } from "./resizable";
+export { default as Badge } from "./badge";
+export { default as Button } from "./button";
+export { default as Input } from "./input";
+export { default as Skeleton } from "./skeleton";
+
